9950a5e26c007003ce98a1546117d4aef66cdaf5
mypv1708 <thanhmy17082001@gmail.com>
2024-11-22 14:19:59 +0700

    Adding cross

--- src/main/java/com/spring/ai4life/controller/AdminController.java
+++ src/main/java/com/spring/ai4life/controller/AdminController.java
@@ -4,11 +4,13 @@ import com.spring.ai4life.common.BaseResponse;
 import com.spring.ai4life.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/api/admin")
 public class AdminController {
     @Autowired
--- src/main/java/com/spring/ai4life/controller/CustomerController.java
+++ src/main/java/com/spring/ai4life/controller/CustomerController.java
@@ -5,12 +5,10 @@ import com.spring.ai4life.dto.request.CustomerRegisterRequest;
 import com.spring.ai4life.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/api/customer")
 public class CustomerController {
     @Autowired
